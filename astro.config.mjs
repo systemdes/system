42a4bfd8375dc2051bccd10190685ae60654bcb5
dandevri <hi@dandevri.es>
2024-09-15 12:38:40 +0200

    Add shiki code highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "github-light",
+      defaultColor: false,
+      wrap: true,
+    },
+  },
 });
